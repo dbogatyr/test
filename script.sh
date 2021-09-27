3ebcec1f69455849fb16857460e8bbd4c8f2ed48
dbogatyr <82878044+dbogatyr@users.noreply.github.com>
2021-09-27 11:45:55 +0300

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,2 +1,13 @@
 #!/bin/bash
-echo "export this.$RC_SQL_USERNAME_LOCAL" >> check.export
+
+cat > destfile <<-EOF
+module.exports = {
+  apps : [{
+    name   : "app",
+    script : "dist/app.js",
+    env_prod: {
+       RC_SQL_USERNAME_LOCALRODUCTION: "this.$RC_SQL_USERNAME_LOCALRODUCTION"
+    },
+  }]
+}
+EOF

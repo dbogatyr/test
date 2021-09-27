242e8ed460e2af3f2b0beb5840c31dfc8c666e79
dbogatyr <82878044+dbogatyr@users.noreply.github.com>
2021-09-27 13:30:36 +0300

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,15 +1,24 @@
 #!/bin/bash
 
-
-cat > destfile <<-EOF
+env > env.file
+cat > ecosystem.config.js <<-EOF
 module.exports = {
   apps : [{
     name   : "app",
     script : "dist/app.js",
     env_prod: {
-       RC_SQL_USERNAME_LOCAL: "$RC_SQL_USERNAME_LOCAL"
+       RC_SQL_USERNAME_LOCAL: "$RC_SQL_USERNAME_LOCAL",
+       RC_SQL_PASSWORD_LOCAL: "$RC_SQL_PASSWORD_LOCAL",
+       RC_SQL_DATABASE_LOCAL: "$RC_SQL_DATABASE_LOCAL",
+       RC_SQL_HOST_LOCAL: "$RC_SQL_HOST_LOCAL",
+       POSTGRES_HOST: "$POSTGRES_HOST",
+       POSTGRES_PASSWORD: "$POSTGRES_PASSWORD",
+       POSTGRES_USER: "$POSTGRES_USER",
+       POSTGRES_PORT: "$POSTGRES_PORT",
+       POSTGRES_DATABASE: "$POSTGRES_DATABASE",
+       LOGZIO_HOST: "$LOGZIO_HOST",
+       LOGZIO_TOKEN: "$LOGZIO_TOKEN"
     },
   }]
 }
 EOF
-echo "export $RC_SQL_USERNAME_LOCAL" >> destfile
